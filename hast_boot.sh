4068af79b7f7c682c60feed019b0c634fb90c3d9
Chi-Yuan Tseng <cytseng@gmail.com>
2020-07-26 21:55:04 +0800

    bug fix for hast_boot.sh, hast_monitor.sh

--- hast_boot.sh
+++ hast_boot.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-# please change
-_member='11'
-
-if [ "$_member" = 'XX' ]; then
-    echo 'Please change the value of _member first.'
-    exit 1
-fi
+. ./config.sh
 
 echo $PATH
 for i in sbin bin
@@ -31,16 +25,15 @@ log="local0.debug"
 name="boot-check"
 
 _ping=`ping -c 1 -t 1 $_peer | grep '100.0% packet loss'` # check alive for peer
-#_ping=`ping -c 1 -t 1 10.140.0.13 | grep '100.0% packet loss'` # peer is dead
 
-_master=`printf "get master$_member\r\n" | nc -N $_memserver $_memport | tr '\r\n' ' ' | sed 's;..*\(10\.1[^ ][^ ]*\)..*;\1;'`
+_master=`printf "get master$master_ip\r\n" | nc -N $_memserver $_memport | tr '\r\n' ' ' | sed 's;..*\(10\.1[^ ][^ ]*\)..*;\1;'`
 
 if [ -n "$_ping" ]; then # change to master
-    printf "set master$_member 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
+    printf "set master$master_ip 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
     echo 'failover master 1'
     $_basedir/hast_failover.sh MASTER $name
 elif [ -z "$_ping" -a "$_master" = "$_thisip" ]; then # change to master
-    printf "set master$_member 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
+    printf "set master$master_ip 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
     echo 'failover master 2'
     $_basedir/hast_failover.sh MASTER $name
 else # change to slave, backup
--- hast_monitor.sh
+++ hast_monitor.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-# please change
-_member='11'
-
-if [ "$_member" = 'XX' ]; then
-    echo 'Please change the value of _member first.'
-    exit 1
-fi
+. ./config.sh
 
 for i in sbin bin
 do
@@ -31,7 +25,7 @@ name="boot-check"
 
 while [ True ]
 do
-    _master=`printf "get master$_member\r\n" | nc -N $_memserver $_memport | tr '\r\n' ' ' | sed 's;..*\(10\.1[^ ][^ ]*\)..*;\1;'`
+    _master=`printf "get master$master_ip\r\n" | nc -N $_memserver $_memport | tr '\r\n' ' ' | sed 's;..*\(10\.1[^ ][^ ]*\)..*;\1;'`
     _curstate=`hastctl status | grep ' primary  */'`
     _ping=`ping -c 1 -t 1 $_peer | grep '100.0% packet loss'` # check alive for peer
 
@@ -40,7 +34,7 @@ do
             echo none 1 && sleep 1 && echo
             continue
         elif [ -z "$_curstate" -a "$_master" = "$_thisip" ]; then
-            printf "set master$_member 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
+            printf "set master$master_ip 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
             echo 'failover master 1'
             $_basedir/hast_failover.sh MASTER $name
         else
@@ -49,11 +43,11 @@ do
         fi
     else
         if [ -z "$_curstate" ]; then
-            printf "set master$_member 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
+            printf "set master$master_ip 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
             echo 'failover master 2'
             $_basedir/hast_failover.sh MASTER $name
         elif [ -n "$_curstate" -a "$_master" != "$_thisip" ]; then
-            printf "set master$_member 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
+            printf "set master$master_ip 0 86400 $_iplen\r\n$_thisip\r\n" | nc -N $_memserver $_memport > /dev/null 2>&1
             echo 'upload'
         else
             echo none 2 && sleep 1 && echo
