99935de0a724aad56955a46aa2b1f23437506873
Chi-Yuan Tseng <cytseng@gmail.com>
2020-07-27 19:45:32 +0800

    bug fix

--- 11.login_1st_node.sh
+++ 11.login_1st_node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./config.sh
 
-gcloud beta --project=$project beta compute ssh --zone "asia-east1-$zone1" "coscup-$username-failover-1"
+gcloud --project=$project beta compute ssh --zone "asia-east1-$zone1" "coscup-$username-failover-1"
--- 12.login_2nd_node.sh
+++ 12.login_2nd_node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./config.sh
 
-gcloud beta --project=$project beta compute ssh --zone "asia-east1-$zone2" "coscup-$username-failover-2"
+gcloud --project=$project beta compute ssh --zone "asia-east1-$zone2" "coscup-$username-failover-2"
