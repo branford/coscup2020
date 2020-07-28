e0f619a52be430baaa0413a5e173e6a32fe380da
Chi-Yuan Tseng <cytseng@gmail.com>
2020-07-28 20:43:15 +0800

    add 13.manual_failover.sh

---
+++ 13.manual_failover.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+. ./config.sh
+
+if [ $# -eq 1 ]; then
+    if [ $1 -eq $master_ip ]; then
+        _num='1'
+        _zone=$zone1
+    elif [ $1 -eq $backup_ip ]; then
+        _num='2'
+        _zone=$zone2
+    else
+        echo 'please give the correct ip'
+    fi
+else
+    _num='1'
+    _zone=$zone1
+fi
+
+gcloud --project=$project beta compute reset --zone "asia-east1-$_zone" "coscup-$username-failover-$_num"
+sleep 1
+gcloud --project=$project beta compute stop --zone "asia-east1-$_zone" "coscup-$username-failover-$_num"
+
